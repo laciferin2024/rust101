b6ef6da30299e9c79dc2f1fce16be59b7614ca60
Hiro <laciferin@gmail.com>
2024-06-18 21:47:19 +0530

    rango: generation complete with secret gen
    Took 9 minutes

--- src/bin/guess_game.rs
+++ src/bin/guess_game.rs
@@ -1,3 +1,22 @@
+use std::io;
+use std::io::Read;
+use rand::Rng;
+
 fn main(){
-    println!("guessing game")
+    println!("Guessing game 2024 by Hiro");
+
+    println!("Input ur guess");
+
+    let secret_number = rand::thread_rng().gen_range(0..100);
+
+    let mut guess = String:: new();
+
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+    println!("Your guess: {}",guess);
+
+    println!("Secret No: {}", secret_number);
+
+    println!("end");
+
 }
