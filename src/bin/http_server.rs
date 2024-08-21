10b8698c754a670542f23b9028d3b7348a88d358
Hiro <laciferin@gmail.com>
2024-08-21 18:17:22 +0530

    /sleep route
    Took 4 minutes

--- src/bin/http_server.rs
+++ src/bin/http_server.rs
@@ -1,7 +1,9 @@
 use std::fmt::format;
-use std::fs;
+use std::{fs, thread};
 use std::net::{TcpListener, TcpStream};
 use std::io::{Read, Write};
+use std::thread::sleep;
+use std::time::Duration;
 
 fn main(){
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -24,12 +26,20 @@ fn handle_connection(mut stream: TcpStream){
     stream.read(&mut buffer).unwrap();
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
-    let acceptedReq = b"GET / HTTP/1.1\r\n";
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
     let (status, inputFile) =
-        if buffer.starts_with(acceptedReq) {
+        if buffer.starts_with(get) {
             ("200 OK","index.html")
             // every statement is separated by a carriage return \r and line \n
-        }else {
+        }else if buffer.starts_with(sleep){
+            println!("sleep");
+            thread::sleep(Duration::from_secs(5));
+            ("200 OK", "index.html")
+        }
+        else {
             ("404 NOT FOUND", "404.html")
         };
 
