0fec92c0cc654f1adba0e19a8e7454ef73e33403
Hiro <laciferin@gmail.com>
2024-08-21 19:00:59 +0530

    wip: threadsafe threadpolol
    Took 20 minutes

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,26 @@
+use std::sync::{Arc, mpsc, Mutex};
 use std::thread;
 
 pub struct ThreadPool{
-    threads: Vec<thread::JoinHandle<()>>,
+   workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
 
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+struct Job;
+
+impl Worker{
+    fn new (id:usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>)->Worker{
+        let thread = thread::spawn(||{
+            receiver;
+        });
+
+        Worker{id,thread}
+    }
 }
 
 impl ThreadPool{
@@ -12,17 +30,24 @@ impl ThreadPool{
     pub fn new(size:usize) -> ThreadPool{
         assert!(size>0);
 
-        let mut threads = Vec::with_capacity(size);
+        let (sender ,receiver) = mpsc::channel();
+        let mut workers = Vec::with_capacity(size);
 
-        for _ in 0..size{
-        //     create threads
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        for id in 0..size{
+            //     create workers
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
-        ThreadPool{threads}
+        ThreadPool{  workers, sender }
     }
     pub fn execute<F>(&self, f:F)
     where
         F:FnOnce()+Send+'static{
+
+        let job  = Box::new(f);
+        self.sender.send(job).unwrap();
 
     }
 }
