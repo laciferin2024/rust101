6ff15a040d36a53785633656e106f05c2a15d779
Hiro <laciferin@gmail.com>
2024-08-25 13:56:18 +0530

    /releases:
    Took 18 minutes

--- src/bin/rocket.rs
+++ src/bin/rocket.rs
@@ -1,17 +1,37 @@
 #[macro_use]
 extern crate rocket;
 
-#[get("/")]
-fn index() -> String {
+use rocket::http::uri::Origin;
+use rocket::response::Redirect;
+use rocket::serde::json::serde_json::json;
+use rocket::serde::json::Value;
+
+const URI_RELEASES_PREFIX: Origin<'static> = uri!("/releases");
+
+#[get("/hello")]
+fn hello() -> String {
     String::from("Hello World")
 }
+#[get("/")]
+fn index() -> Redirect {
+    let msg : Option<&str> = None;
+    Redirect::to(uri!(URI_RELEASES_PREFIX, releases("osx", "v1.0",msg)))
+}
 
-#[get("/releases/<platform>/1.0?<msg>")]
-fn releases(platform:&str, msg: Option<String>)->String{
-    return String::from("Release")
+#[get("/<platform>/<version>?<msg>")]
+fn releases(platform:&str, version:&str,msg: Option<String>)->Value{
+
+
+    json!({
+        "notes": "Works"
+    })
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().
+        mount("/", routes![index]).
+        mount("/hello", routes![hello]).
+        mount(URI_RELEASES_PREFIX.to_string(), routes![releases])
+        // mount("/releases", routes![releases])
 }
