4a3f1ae18d719455b04181c833dae7ab0040051c
Hiro <laciferin@gmail.com>
2024-08-25 17:29:33 +0530

    unsafe: derefencing
    Took 4 minutes

--- src/bin/unsafe.rs
+++ src/bin/unsafe.rs
@@ -1,9 +1,12 @@
 fn main() {
-    let x: i32 = 42;
-    let r: *const i32 = &x;
+    let mut x: i32 = 42;
+    let r: *const i32 = &22;
+    let  blunder: *mut i32 = &mut x;
 
     unsafe {
         println!("The value of x is: {}", *r);
+        *blunder+=1;
+        *blunder;
     // dereference a raw pointer
     }
 }
