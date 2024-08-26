7a8503c26ab842266088f88dad8d4954f0c90ee7
Hiro <laciferin2024@protonmail.com>
2024-08-26 07:21:35 +0530

    Learn/rocket 2 (#2)
    * rocket: get_latest_release
    
    Took 12 minutes
    
    * geraring up for client:
    
    Took 9 minutes
    
    * wire up: error
    
    Took 9 minutes
    
    * no content:
    
    Took 2 minutes
    
    * empty: response
    
    Took 1 minute
    
    * rocket: ready
    
    Took 15 minutes
    
    ---------
    
    Co-authored-by: Nasfame <laciferin@gmail.com>

--- src/bin/rocket.rs
+++ src/bin/rocket.rs
@@ -2,11 +2,15 @@
 extern crate rocket;
 
 use std::fmt::format;
+use futures::{TryFutureExt, TryStreamExt};
+use reqwest::Client;
 use rocket::http::Status;
 use rocket::http::uri::Origin;
+use rocket::outcome::IntoOutcome;
 use rocket::response::Redirect;
 use rocket::serde::json::serde_json::json;
 use rocket::serde::json::Value;
+use rocket::State;
 
 const URI_RELEASES_PREFIX: Origin<'static> = uri!("/releases");
 
@@ -22,24 +26,49 @@ fn hello() -> String {
 }
 
 
+async fn get_latest_release(client: &State<Client>, repo: &str) -> Result<Value, reqwest::Error> {
+    let url = format!("https://api.github.com/repos/{repo}/releases/latest");
+    let response = client.get(&url).send().await?;
+    let github_release = response.json::<Value>().await?;
+    Ok(github_release)
+}
+
+const REPO_GOLANG_AIR: &str ="air-verse/air";
+
 #[get("/<platform>/<version>?<msg>")]
-fn releases(platform:&str, version:&str,msg: Option<String>)->Result<Value,Status>{
+async fn releases(platform:&str, version:&str,msg: Option<String>, client: &State<Client>)->Result<Value,Status>{
 
     if let Some(msg) = msg {
         println!("msg is {msg}");
         return Err(Status::NoContent);
     }
 
-    Ok(json!({
-        "notes": "ready",
-        "platform": platform,
-        "version": version,
-    }))
+    // let response = get_latest_release(client,REPO_GOLANG).await.or(Err(Status::NoContent));
+    let mut response = get_latest_release(client, REPO_GOLANG_AIR).await;
+
+
+    match response{
+        Err(e) => {
+            println!("request errored with {}", e);
+            return Err(Status::NoContent)
+        },
+        Ok(response)=> {
+            Ok(json!({
+                "notes": "ready",
+                "platform": platform,
+                "version": version,
+                "response": response ,
+            }))
+        }
+    }
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build().
+        manage(
+          Client::builder().user_agent("reqwest").build().unwrap()
+        ).
         mount("/", routes![index]).
         mount("/hello", routes![hello]).
         mount(URI_RELEASES_PREFIX.to_string(), routes![releases])
