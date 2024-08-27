a948f063e6e462d2f1cefa8d8e1c28c7a3f08111
Nasfame <laciferin@gmail.com>
2024-08-27 08:27:02 +0530

    poem: ready just TcpListener issue
    Took 15 minutes

---
+++ src/bin/poem.rs
@@ -0,0 +1,27 @@
+use poem::Route;
+use poem::web::Query;
+use poem_openapi::{OpenApi, OpenApiService};
+use poem_openapi::payload::PlainText;
+use tokio::net::TcpListener;
+
+struct Api;
+// mod user;
+
+#[OpenApi]
+impl Api{
+    #[oai(path="/hello", method = "get")]
+    async fn index(&self, name: Query<Option<String>>)->PlainText<String>{
+        match name.0 {
+            Some(name) =>PlainText(format!("hello, {}",name)),
+            None=>PlainText("hello!".to_string()),
+        }
+    }
+}
+
+#[tokio::main]
+async fn main()->Result<(), std::io::Error>{
+    let api_service = OpenApiService::new(Api, "hello world", "1.0").server("http://localhost:3001/api");
+    let ui = api_service.swagger_ui();
+    let app = Route::new().nest("/api", api_service).nest("/",ui);
+    poem::Server::new(TcpListener::bind("127.0.0.1:3001")).run(app).await
+}
