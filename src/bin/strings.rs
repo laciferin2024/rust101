6c8d64e187aac88ebc71e199a0f15c8d62fc254a
Nasfame <laciferin@gmail.com>
2024-08-28 19:10:00 +0530

    shown example not working any more
    Took 7 minutes

---
+++ src/bin/strings.rs
@@ -0,0 +1,22 @@
+use std::sync::Arc;
+use std::thread;
+
+fn main() {
+    let multi_txt = String::from("Read by multiple threads");
+    let multi_slice = &multi_txt[..]; //doesn't have static lifetime
+
+    let shared_txt: Arc<&str> = Arc::from(multi_slice);
+    let mut handles = vec![];
+
+    for _ in 0..3 {
+        let text_ref = Arc::clone(&shared_txt);
+        let handle = thread::spawn(move || {
+            println!("Thread is reading {text_ref}");
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+}
