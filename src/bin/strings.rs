2780726373826c3ac381b5a59ce02d3db85630b7
Hiro <laciferin2024@protonmail.com>
2024-08-29 06:52:33 +0530

    Learn/strings (#4)
    * shown example not working any more
    
    Took 7 minutes
    
    * wip:
    
    Took 6 minutes
    
    * just: tried crazy stuffs
    
    Took 16 minutes
    
    * rust: ci
    
    Took 3 minutes
    
    * add: rust action
    
    Took 3 minutes
    
    * cargo fix --bin "http_server"
    
    Took 53 seconds
    
    * Build bins
    
    Took 3 minutes
    
    * shell: bash
    
    Took 51 seconds
    
    * needs: build
    
    Took 17 seconds
    
    * workflow: rust binaries ci
    
    Took 2 minutes
    
    * concurrency: ci
    
    Took 2 minutes
    
    * fixups:
    
    Took 1 minute
    
    * fixed the issue:
    
    Took 10 minutes
    
    * strings:
    
    Took 59 seconds
    
    * read by::
    
    Took 26 seconds
    
    ---------
    
    Co-authored-by: Nasfame <laciferin@gmail.com>

---
+++ src/bin/strings.rs
@@ -0,0 +1,35 @@
+use std::sync::Arc;
+use std::thread;
+
+// static multi_txt: String = String::from("Read by multiple threads");
+
+fn main() {
+    // let multi_txt = String::from("Read by multiple threads");
+    // let multi_slice = &multi_txt[..]; //doesn't have static lifetime
+    //
+    let multi_txt = Arc::new("Read by multiple threads");
+    let multi_slice = &multi_txt[..]; //doesn't have static lifetime
+
+    // let multi_slice = move || -> &str{
+    //     &multi_txt[..]
+    // }();
+
+
+    println!("multi_slice={multi_slice}");
+
+    let shared_txt: Arc<&str> = Arc::from(multi_slice);
+    let mut handles = vec![];
+
+    for _ in 0..3 {
+        let text_ref = Arc::clone(&shared_txt);
+        let handle = thread::spawn(move || {
+            println!("Thread is reading \"{text_ref}\" ");
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+}
