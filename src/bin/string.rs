19cf43ab54c8c250b6a0a8090cbffdac5460e29d
Hiro <laciferin2024@protonmail.com>
2024-08-29 06:06:23 +0530

    Learn/poem (#3)
    * add: poem deps
    
    Took 2 minutes
    
    * poem: ready just TcpListener issue
    
    Took 15 minutes
    
    * import fixed:
    
    Took 24 seconds
    
    * poem: ready and working
    
    Took 7 minutes
    
    * wip: debug fixing
    
    Took 4 minutes
    
    * wip:
    
    Took 3 minutes
    
    * wip: fixing:
    
    Took 3 minutes
    
    * still: geting
    
    Took 7 minutes
    
    * added: hi route and its working
    
    Took 2 minutes
    
    * wip: path issue; expected map
    
    Took 7 minutes
    
    * string; types learn
    
    Took 3 minutes
    
    * fixed: all isues
    
    Took 12 minutes
    
    * hello:
    
    Took 17 seconds
    
    * created: mod user
    
    Took 5 minutes
    
    * add: mod user
    
    Took 8 minutes
    
    * api: user svc
    
    Took 2 minutes
    
    * post: User ready
    
    Took 2 minutes
    
    ---------
    
    Co-authored-by: Nasfame <laciferin@gmail.com>

---
+++ src/bin/string.rs
@@ -0,0 +1,21 @@
+use std::rc::Rc;
+
+fn get_greeting(hour:u8) -> &'static str{
+	if hour<12{
+		"Good Morning!"
+	}else if hour<18{
+        "Good Afternoon"
+    }else{
+	"Good Evening"
+    }
+}
+
+fn main(){
+ let greet = get_greeting(12);
+println!("greet = {greet}");
+
+    let large_text: &'static str = "Hey..... this function's return type contains a borrowed value, but there is no value for it to be borrowed from
+help: consider using the `'static` lifetime, but this is uncommon unless you're returning a borrowed value from a `const` or a `static`";
+let rc_str = Rc::from(&large_text[4..24]);
+
+}
